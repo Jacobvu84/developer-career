c769b15bbc4bd1236f2cc2bb72d84ed6087d8eef
JacobVu84 <vuthelinh@gmail.com>
2022-09-14 17:11:19 +0700

    Using @PutMapping to update product

--- src/main/java/jacob/vu/coffee/repositories/ProductRepository.java
+++ src/main/java/jacob/vu/coffee/repositories/ProductRepository.java
@@ -10,4 +10,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // Name method needs to follow convention: findBy<nameAttribute>
     List<Product> findByName(String name);
 
+    List<Product> findByProductId(Long id);
+
 }
